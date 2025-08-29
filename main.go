59ce643667f1dbfbdbac8ba604b8e35a84867e55
ing-jjarmenta <ing.jjarmenta@gmail.com>
2025-08-29 11:39:53 -0500

    Serve básico

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
-func main()  {
-	fmt.Println("Hola, Go!")
+func main() {
+	http.HandleFunc("/api/hello", helloHandler)
+
+	log.Println("Server 8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Println("Error desde el serve")
+		log.Fatal(err)
+	}
+
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{"message": "Hola desde la API"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
